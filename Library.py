2a1adeb0122bf95ce0a939761168bdc3a5ee593f
Kenny Zhu Ye <kennyzhuye@Kennys-MacBook-Air.local>
2022-09-13 09:37:09 -0500

    put

---
+++ Library.py
@@ -0,0 +1,37 @@
+class Library:
+    def __init__(self, books):
+        self.books = books
+
+    def disponibilidad(self):
+        print(f"\n{len(self.books)} los libros disponibles son: ")
+        for book in self.books:
+            print(" - " + book)
+        print("\n")
+
+    def prestar(self, name, bookname):
+        if bookname not in self.books:
+            print(
+                f"{bookname} el libro no esta disponible.\n")
+        else:
+            track.append({name: bookname})
+            print("libro prestado, por favor devuelvelo, cuando no lo vas a usar.\n")
+            self.books.remove(bookname)
+
+    def devuelto(self, bookname):
+        print("libro devuelto, muchas gracias \n")
+        self.books.append(bookname)
+
+
+class Usuario():
+    def solicitar(self):
+        self.book = input("ingresa el libro que quiere pedir:  ")
+        return self.book
+
+    def devolver(self):
+        name = input("ingresa el nombre: ")
+        self.book = input("ingresa el nombre del libro que quiere devolver:  ")
+        track = []
+        if {name: self.book} in track:
+            track.remove({name: self.book})
+        return self.book
+
---
+++ codetorun.py
@@ -0,0 +1,10 @@
+import Library 
+
+if __name__ == "__main__":
+
+    Delhilibrary = Library(
+        ["vistas", "invention", "rich&poor", "indian", "macroeconomics", "microeconomics"])
+    student = Student()
+    track = []
+
+    print("""Escoge una opcion-\n1. mostrar\n2. prestar\n3. devolver\n4. salir\n""")
